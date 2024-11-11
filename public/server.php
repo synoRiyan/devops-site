732f826d383bac60d1c76bf35a21ee11993ba96b
sheikhri <riyan.sheikh@synoverge.com>
2024-11-11 13:46:01 +0530

    asdf

--- public/server.php
+++ public/server.php
@@ -4,4 +4,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $message = htmlspecialchars($_POST['message']);
     echo "Thank you, $name. Your message has been received: $message";
 }
+
+
+if (getenv('FEATURE_NEW_CONTACT_FORM') === 'true') {
+    // New contact form feature is enabled
+    echo "New Contact Form!";
+} else {
+    // Old contact form feature
+    echo "Old Contact Form!";
+}
+
+
 ?>
