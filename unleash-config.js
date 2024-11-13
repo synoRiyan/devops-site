696849f21ad45bbfd5f3aaa26801e4239e1c20f9
sheikhri <riyan.sheikh@synoverge.com>
2024-11-13 11:13:45 +0530

    asdfasdf

---
+++ unleash-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    databaseUrl: 'postgres://unleash_user:password@localhost:5432/unleash',
+    port: 4242,
+    unleashSecret: 'riyan@123', // This is your API key for Unleash server
+};
